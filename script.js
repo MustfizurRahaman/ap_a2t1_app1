23a16ce200ba300d05cb6a32b633e50324090526
Mustafizur Rahaman <61288061+MustfizurRahaman@users.noreply.github.com>
2024-01-30 19:58:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ require(["esri/views/MapView", "esri/WebMap"], (MapView, WebMap) => {
       id: "4d595b39dfcd4b53bb87f3a07ad03e8b"
     }
   });
-
   const view = new MapView({
     map: webmap,
     container: "viewDiv"
