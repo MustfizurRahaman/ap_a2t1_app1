2819c3bd1ee42655ee14b986d9d40ecea6bafc83
Mustafizur Rahaman <61288061+MustfizurRahaman@users.noreply.github.com>
2024-01-30 19:55:46 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+require(["esri/views/MapView", "esri/WebMap"], (MapView, WebMap) => {
+    portalItem: {
+      // autocasts as new PortalItem()
+      id: "4d595b39dfcd4b53bb87f3a07ad03e8b"
+    }
+  });
+
+  const view = new MapView({
+    map: webmap,
+    container: "viewDiv"
+  });
+});
